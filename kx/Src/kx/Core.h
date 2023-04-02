5b6b8e09d63b21c9482917cb18d06ae283a4c1ef
uttkkarshh <utkarshtiwari.437@gmail.com>
2023-04-02 11:39:19 +0530

    premake

--- kx/Src/kx/Core.h
+++ kx/Src/kx/Core.h
@@ -1,6 +1,6 @@
 
 #ifdef kx_PLATFORM_WINDOW
-#ifdef HZ_BUILD_DLL
+#ifdef kx_BUILD_DLL
 #define kx_API _declspec(dllexport)
 #else
 #define kx_API _declspec(dllimport)
---
+++ premake5.lua
@@ -0,0 +1,91 @@
+workspace "kx"
+   architecture "x64"
+   configurations { "Debug", "Release","Dist" }
+
+outputdir = "%{cfg.bhidcfg}-%{cfg.system}-%{cfg.architecture}"
+project "kx"
+   location "kx"
+   kind "SharedLib"
+   language "C++"
+
+
+   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("bin-int/" ..outputdir .. "/%{prj.name}")
+
+   files
+   {
+	   "%{prj.name}/Src/**.h",
+	    "%{prj.name}/Src/**.cpp"
+   }
+
+   includedirs{
+	   "%{prj.name}/vendor/spdlog/include"
+
+   }
+   filter "system:windows"
+      cppdialect "c++17"
+	  staticruntime "On"
+	  systemversion "latest"
+
+      defines{
+	   "kx_PLATFORM_WINDOW",
+	   "kx_BUILD_DLL"
+      }
+      
+   filter "configurations:Debug"
+         defines "kx_DEBUG"
+	    
+   filter "configurations:Release"
+         defines "kx_RELEASE"
+		 optimize "On"
+
+   filter "configurations:Dist"
+         defines "kx_Dist"
+		 optimize "On"
+
+
+
+
+
+   project "sand"
+	location "sand"
+    kind "ConsoleApp" 
+    language "C++"
+
+
+   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("bin-int/" ..outputdir .. "/%{prj.name}")
+
+   files
+   {
+	   "%{prj.name}/Src/**.h",
+	    "%{prj.name}/Src/**.cpp"
+   }
+
+   includedirs{
+	   "kx/vendor/spdlog/include",
+	   "kx/Src"
+   }
+   links{
+	   "kx"
+   }
+   filter "system:windows"
+      cppdialect "c++17"
+	  staticruntime "On"
+	  systemversion "latest"
+
+      defines{
+	   "kx_PLATFORM_WINDOW"
+	   
+      }
+    
+   filter "configurations:Debug"
+         defines "kx_DEBUG"
+	    
+   filter "configurations:Release"
+         defines "kx_RELEASE"
+		 optimize "On"
+
+   filter "configurations:Dist"
+         defines "kx_Dist"
+		 optimize "On"
